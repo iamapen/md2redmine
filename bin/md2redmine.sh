32ecf30185e4ba3a339765bc718ef1aa3da193d0
pen <iamapen@studiopoppy.com>
2019-06-19 01:44:45 +0900

    Change: STDOUTに出力するようにした

--- bin/md2redmine.sh
+++ bin/md2redmine.sh
@@ -2,30 +2,25 @@
 THIS_SCRIPT=$(readlink $0)
 BASE_DIR=$(cd $(dirname "$THIS_SCRIPT")/..;pwd)
 
-VERSION=0.1.0
+VERSION=1.0.0
 
 function usage() {
   cat <<__EOF__
 Markdown to Redmine-wiki Converter - ver $VERSION
 
-Usage: $0 <input md> [output]
+Usage: $0 <input md>
   ex) $0 input.md
-  ex) $0 input.md output.txt
+  ex) $0 input.md > output.txt
 
 __EOF__
 }
 
-INPUT=$1
-FILENAME=$(basename "$INPUT")
-
 if [ $# -eq 1 ]; then
-  OUTPUT=$FILENAME.textile
-elif [ $# -eq 2 ]; then
-  OUTPUT=$2
+  INPUT=$1
 else
   usage
   exit 1
 fi
 
-pandoc --from=gfm --to=textile "$INPUT" -o "$OUTPUT" --filter="$BASE_DIR/src/filters/md2redmine.js"
+pandoc --from=gfm --to=textile "$INPUT" --filter="$BASE_DIR/src/filters/md2redmine.js"
 
